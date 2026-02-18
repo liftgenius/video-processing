f40c5a73fec8b9f6b4abb2ce94021fb906cc10b4
John Nevin <john@liftgeni.us>
2026-02-18 12:20:41 -0500

    Use .env file for variables
    Sensitive and/or dynamic variables are read from a root .env file
    using dotenv and referenced by their CONST variable names in all
    functions.

--- lib/lambda_function.py
+++ lib/lambda_function.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-# LAMBDA_URL = os.environ.get('LAMBDA_URL')
-LAMBDA_URL = "http://127.0.0.1:3000/detect_bar"
+ENVIRONMENT = os.getenv('ENVIRONMENT')
+LAMBDA_INFERENCE_BUCKET_NAME = os.environ.get('LAMBDA_INFERENCE_BUCKET_NAME')
+LAMBDA_INFERENCE_URL = os.environ.get('LAMBDA_INFERENCE_URL')
+LOCALSTACK_HOST = os.environ.get('LOCALSTACK_HOST')
 
 
 def convert_nd_array_to_image(frame):
@@ -22,7 +24,7 @@ def convert_nd_array_to_image(frame):
 
 def invoke_lambda(presigned_url):
     try:
-        response = requests.post(LAMBDA_URL, json={"url": presigned_url})
+        response = requests.post(LAMBDA_INFERENCE_URL, json={"url": presigned_url})
         data = response.json()
         print(f" λ {data}")
         if "pred_boxes" in data and len(data["pred_boxes"]) > 0:
@@ -40,19 +42,20 @@ def get_inference(frame, name):
 
     file = convert_nd_array_to_image(frame)
     # bucket_name = os.environ.get('PROD_IMAGE_BUCKET')
-    bucket_name = "image-uploads"
+    bucket_name = LAMBDA_INFERENCE_BUCKET_NAME
     object_name = name + ".jpg"
 
     if file != None:
 
-        uploaded_object = boto3_utils.upload_object(bucket_name, object_name, file)
+        _uploaded_object = boto3_utils.upload_object(bucket_name, object_name, file)
         presigned_url = boto3_utils.create_presigned_url(bucket_name, object_name)
-        # FOR USE WHEN USING LOCALSTACK S3 with SAM LAMBDA
+        # WHEN USING LOCALSTACK S3 with SAM LAMBDA
         # REPLACE THE HOSTNAME WITH THE IP OF THE DOCKER CONTAINER RUNNING LOCALSTACK
         # docker inspect localstack-main
-        edited_url = str(presigned_url).replace("localhost.localstack.cloud", "172.17.0.2")
-        # inference = invoke_lambda(presigned_url)
-        inference = invoke_lambda(edited_url)
+        if ENVIRONMENT == "local":
+            presigned_url = str(presigned_url).replace("localhost.localstack.cloud", LOCALSTACK_HOST)
+
+        inference = invoke_lambda(presigned_url)
         boto3_utils.delete_object(bucket_name=bucket_name, object_name=object_name)
         return inference
 
--- threaded_worker.py
+++ threaded_worker.py
@@ -1,6 +1,25 @@
 import functools
 import functools, time, threading, json
+import os
 from lib import track_bar, boto3_utils, rabbitmq_utils
+from dotenv import load_dotenv, find_dotenv
+
+env_loaded = load_dotenv(find_dotenv())
+print(f"Successfully loaded .env: {env_loaded}")
+# exit(0)
+
+# SQS_AWS_PROFILE = os.environ.get('SQS_AWS_PROFILE')
+# S3_AWS_PROFILE = os.environ.get('S3_AWS_PROFILE')
+
+RABBITMQ_USERNAME = os.environ.get('RABBITMQ_USERNAME')
+RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD')
+RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST')
+RABBITMQ_OUTGOING_EXCHANGE = os.environ.get('RABBITMQ_OUTGOING_EXCHANGE')
+RABBITMQ_OUTGOING_QUEUE = os.environ.get('RABBITMQ_OUTGOING_QUEUE')
+RABBITMQ_INCOMING_EXCHANGE = os.environ.get('RABBITMQ_INCOMING_EXCHANGE')
+RABBITMQ_INCOMING_QUEUE = os.environ.get('RABBITMQ_INCOMING_QUEUE')
+# DEFAULT_EXPIRATION = os.environ.get('DEFAULT_EXPIRATION')
+
 
 
 def process_video(conn, ch, delivery_tag, data):
@@ -10,23 +29,23 @@ def process_video(conn, ch, delivery_tag, data):
     # thread_id = threading.get_ident()
     message = json.dumps({"job_id": job_id, "status": "analyzing"})
     rabbitmq_utils.send_message(
-        exchange_name="processing_jobs_exchange",
-        queue_name="processing_jobs_queue",
-        routing_key_name="processing_jobs_queue", 
+        exchange_name=RABBITMQ_OUTGOING_EXCHANGE,
+        queue_name=RABBITMQ_OUTGOING_QUEUE,
+        routing_key_name=RABBITMQ_OUTGOING_QUEUE, 
         message=message
     )
     url = boto3_utils.create_presigned_url(bucket_name=bucket, object_name=key)
     print(f" \n 🗓️ {time.strftime('%l:%M:%S%p %Z on %b %d, %Y')}")
     print(f" 💼 Job: {job_id}")
     print(f" 🎥 Processing video...")
-    output_url = track_bar.analyze(job_id, url)
+    output_url = track_bar.analyze(RABBITMQ_OUTGOING_EXCHANGE, RABBITMQ_OUTGOING_QUEUE, job_id, url)
     if output_url == -1:
         message = json.dumps({"job_id": job_id, "status": "failure"})
         print(" ❌ Could not process video\n")
         rabbitmq_utils.send_message(
-            exchange_name="processing_jobs_exchange",
-            queue_name="processing_jobs_queue",
-            routing_key_name="processing_jobs_queue", 
+            exchange_name=RABBITMQ_OUTGOING_EXCHANGE,
+            queue_name=RABBITMQ_OUTGOING_QUEUE,
+            routing_key_name=RABBITMQ_OUTGOING_QUEUE, 
             message=message
         )
         # print(' \n 📧 Waiting for messages. To exit press CTRL+C')
@@ -44,9 +63,9 @@ def process_video(conn, ch, delivery_tag, data):
             }
         )
         rabbitmq_utils.send_message(
-            exchange_name="processing_jobs_exchange",
-            queue_name="processing_jobs_queue",
-            routing_key_name="processing_jobs_queue", 
+            exchange_name=RABBITMQ_OUTGOING_EXCHANGE,
+            queue_name=RABBITMQ_OUTGOING_QUEUE,
+            routing_key_name=RABBITMQ_OUTGOING_QUEUE, 
             message=message
         )
         cb = functools.partial(rabbitmq_utils.ack_message, ch, delivery_tag)
@@ -76,21 +95,21 @@ def on_message(ch, method_frame, _header_frame, body, args):
         conn.add_callback_threadsafe(cb)
 
 
-credentials = rabbitmq_utils.get_plain_credentials('liftgenius', 'L0c@lY0k3l1509!')
+credentials = rabbitmq_utils.get_plain_credentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
 parameters = rabbitmq_utils.get_parameters(credentials=credentials)
 threads = []
 
 consumer_connection, consumer_channel = \
     rabbitmq_utils.setup_channel(
         parameters=parameters, 
-        exchange_name="incoming_jobs_exchange", 
-        queue_name="incoming_jobs_queue", 
-        routing_key_name="incoming_jobs_queue"
+        exchange_name=RABBITMQ_INCOMING_EXCHANGE, 
+        queue_name=RABBITMQ_INCOMING_QUEUE, 
+        routing_key_name=RABBITMQ_INCOMING_QUEUE
     )
 
 on_message_callback = functools.partial(on_message, args=(consumer_connection, threads))
 consumer_channel.basic_qos(prefetch_count=1)
-consumer_channel.basic_consume("incoming_jobs_queue", on_message_callback)
+consumer_channel.basic_consume(RABBITMQ_INCOMING_QUEUE, on_message_callback)
 try:
     consumer_channel.start_consuming()
     print(' \n 📧 Waiting for messages. To exit press CTRL+C')
